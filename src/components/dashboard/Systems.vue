0815868faa54b2d313c206bf46722ec524f802fb
Jihad <gehadsaed199@gmail.com>
2025-05-07 22:25:00 +0300

    commit for Permission in  reservation & placement-test

--- src/components/dashboard/Systems.vue
+++ src/components/dashboard/Systems.vue
@@ -58,13 +58,13 @@ const systems = [
     name: "Reservation System",
     route: "/reservation",
     image: reservation ,
-    requiresPermission: "view-reservation", 
+    // requiresPermission: "view-reservation", 
   },
   {
     name: "Placement Test",
     route: "/placement-test",
     image: Placement ,
-    requiresPermission: "view-placement-test", 
+    // requiresPermission: "view-placement-test", 
   },
 ];
 
